394bc6b1f1273b6e0841418189d977d4809eb740
costa-rica <nrodrig1@gmail.com>
2023-08-04 16:12:37 +0200

    HomeVC screen modified to look better with scroll and size of buttons, spacing, etc;

--- TuRincon/Views/HomeVC.swift
+++ TuRincon/Views/HomeVC.swift
@@ -46,8 +46,9 @@ class HomeVC: DefaultViewController {
         
         setup_vwVCHeaderOrange()
         setup_vwVCHeaderOrangeTitle()
-        setup_vwEtymology()
         setup_stckVwHome()
+        setup_vwEtymology()
+        
 //        setup_btnToRegister()
 //        setup_btnToLogin()
         print("Documents Directory: ", FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last ?? "Not Found!")
@@ -105,87 +106,13 @@ class HomeVC: DefaultViewController {
         
     }
     
-    func setup_vwEtymology(){
-        vwEtymologyBackground.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(vwEtymologyBackground)
-        vwEtymologyBackground.accessibilityIdentifier = "vwEtymologyBackground"
-        vwEtymologyBackground.backgroundColor = UIColor(named: "gray-500")
-        vwEtymologyBackground.topAnchor.constraint(equalTo: vwVCHeaderOrangeTitle.bottomAnchor, constant: heightFromPct(percent: cardTopSpacing)).isActive=true
-        vwEtymologyBackground.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: widthFromPct(percent: cardInteriorPadding)).isActive=true
-        vwEtymologyBackground.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: widthFromPct(percent: cardInteriorPadding * -1)).isActive=true
-        vwEtymologyBackground.heightAnchor.constraint(equalToConstant: heightFromPct(percent: 52)).isActive=true
-        vwEtymologyBackground.layer.cornerRadius = 10
-        
-        let lblEtymologyTitle = UILabel()
-        lblEtymologyTitle.text = "Rincón"
-        lblEtymologyTitle.font = UIFont(name: "Rockwell_tu", size: 20)
-        vwEtymologyBackground.addSubview(lblEtymologyTitle)
-        lblEtymologyTitle.accessibilityIdentifier = "lblEtymologyTitle"
-        lblEtymologyTitle.translatesAutoresizingMaskIntoConstraints=false
-        lblEtymologyTitle.topAnchor.constraint(equalTo: vwEtymologyBackground.topAnchor, constant: heightFromPct(percent: 2.5)).isActive=true
-        lblEtymologyTitle.leadingAnchor.constraint(equalTo: vwEtymologyBackground.leadingAnchor, constant: widthFromPct(percent: 5)).isActive=true
-        
-        let lblEtymology = UILabel()
-        lblEtymology.text = "Etymology"
-        lblEtymology.font = UIFont(name: "Rockwell-Bold_tu", size: 20)
-        vwEtymologyBackground.addSubview(lblEtymology)
-        lblEtymology.accessibilityIdentifier = "lblEtymology"
-        lblEtymology.translatesAutoresizingMaskIntoConstraints=false
-        lblEtymology.topAnchor.constraint(equalTo: lblEtymologyTitle.bottomAnchor, constant: heightFromPct(percent: 2.5)).isActive=true
-        lblEtymology.leadingAnchor.constraint(equalTo: vwEtymologyBackground.leadingAnchor, constant: widthFromPct(percent: cardInteriorPadding)).isActive=true
-        
-        let imgVwLine01 = createDividerLine(thicknessOfLine: 2)
-        vwEtymologyBackground.addSubview(imgVwLine01)
-        imgVwLine01.translatesAutoresizingMaskIntoConstraints=false
-        imgVwLine01.topAnchor.constraint(equalTo: lblEtymology.bottomAnchor, constant: heightFromPct(percent: 2.5)).isActive=true
-        imgVwLine01.leadingAnchor.constraint(equalTo: vwEtymologyBackground.leadingAnchor, constant: widthFromPct(percent: cardInteriorPadding)).isActive=true
-        imgVwLine01.trailingAnchor.constraint(equalTo: vwEtymologyBackground.trailingAnchor, constant: widthFromPct(percent: cardInteriorPadding * -1)).isActive=true
-        
-        
-        let lblEtymologyDef = UILabel()
-        lblEtymologyDef.text = "Spanish rincón, literally, corner, nook, alteration of recón, rencón from Arabic dialect (Spain) rukun (Arabic rukn)"
-        lblEtymologyDef.numberOfLines=0
-        lblEtymologyDef.lineBreakMode = .byWordWrapping
-        lblEtymologyDef.font = UIFont(name: "Rockwell_tu", size: 20)
-        vwEtymologyBackground.addSubview(lblEtymologyDef)
-        lblEtymologyDef.translatesAutoresizingMaskIntoConstraints=false
-        lblEtymologyDef.topAnchor.constraint(equalTo: imgVwLine01.bottomAnchor, constant: heightFromPct(percent: 2.5)).isActive=true
-        lblEtymologyDef.leadingAnchor.constraint(equalTo: vwEtymologyBackground.leadingAnchor, constant: widthFromPct(percent: cardInteriorPadding)).isActive=true
-        lblEtymologyDef.trailingAnchor.constraint(equalTo: vwEtymologyBackground.trailingAnchor, constant: widthFromPct(percent: cardInteriorPadding * -1)).isActive=true
-        
-        
-        let lblFirstKnown = UILabel()
-        lblFirstKnown.text = "First Known Use"
-        lblFirstKnown.font = UIFont(name: "Rockwell-Bold_tu", size: 20)
-        vwEtymologyBackground.addSubview(lblFirstKnown)
-        lblFirstKnown.translatesAutoresizingMaskIntoConstraints=false
-        lblFirstKnown.topAnchor.constraint(equalTo: lblEtymologyDef.bottomAnchor, constant: heightFromPct(percent: 5)).isActive=true
-        lblFirstKnown.leadingAnchor.constraint(equalTo: vwEtymologyBackground.leadingAnchor, constant: widthFromPct(percent: cardInteriorPadding)).isActive=true
-        
-        let imgVwLine02 = createDividerLine(thicknessOfLine: 2)
-        vwEtymologyBackground.addSubview(imgVwLine02)
-        imgVwLine02.translatesAutoresizingMaskIntoConstraints=false
-        imgVwLine02.topAnchor.constraint(equalTo: lblFirstKnown.bottomAnchor, constant: heightFromPct(percent: 2.5)).isActive=true
-        imgVwLine02.leadingAnchor.constraint(equalTo: vwEtymologyBackground.leadingAnchor, constant: widthFromPct(percent: cardInteriorPadding)).isActive=true
-        imgVwLine02.trailingAnchor.constraint(equalTo: vwEtymologyBackground.trailingAnchor, constant: widthFromPct(percent: cardInteriorPadding * -1)).isActive=true
-        
-        let lblDate = UILabel()
-        lblDate.text = "1589"
-        lblDate.font = UIFont(name: "Rockwell_tu", size: 20)
-        vwEtymologyBackground.addSubview(lblDate)
-        lblDate.translatesAutoresizingMaskIntoConstraints=false
-        lblDate.topAnchor.constraint(equalTo: imgVwLine02.topAnchor, constant: heightFromPct(percent: 2.5)).isActive=true
-        lblDate.leadingAnchor.constraint(equalTo: vwEtymologyBackground.leadingAnchor, constant: widthFromPct(percent: cardInteriorPadding)).isActive=true
-    }
-    
     func setup_stckVwHome(){
         print("- setup_stckVwHome")
         scrllVwHome.translatesAutoresizingMaskIntoConstraints=false
         view.addSubview(scrllVwHome)
         scrllVwHome.accessibilityIdentifier="scrllVwHome"
-//        scrllVwHome.backgroundColor = .cyan
         
-        scrllVwHome.topAnchor.constraint(equalTo: vwEtymologyBackground.bottomAnchor, constant: heightFromPct(percent: 1)).isActive=true
+        scrllVwHome.topAnchor.constraint(equalTo: vwVCHeaderOrangeTitle.bottomAnchor, constant: heightFromPct(percent: cardTopSpacing)).isActive=true
         scrllVwHome.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: widthFromPct(percent: cardInteriorPadding)).isActive=true
         scrllVwHome.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: widthFromPct(percent: cardInteriorPadding * -1)).isActive=true
 //        scrllVwAdmin.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive=true
@@ -206,11 +133,108 @@ class HomeVC: DefaultViewController {
 //        stckVwHome.trailingAnchor.constraint(equalTo: scrllVwHome.trailingAnchor).isActive=true
         stckVwHome.bottomAnchor.constraint(equalTo: scrllVwHome.bottomAnchor).isActive=true
 //        stckVwHome.backgroundColor = .black
-        stckVwHome.spacing = 8
+        stckVwHome.spacing = 20
         setup_btnToRegister()
         setup_btnToLogin()
         setup_pickerApi()
     }
+    
+    func setup_vwEtymology(){
+
+        
+        vwEtymologyBackground.translatesAutoresizingMaskIntoConstraints = false
+//        view.addSubview(vwEtymologyBackground)
+        stckVwHome.insertArrangedSubview(vwEtymologyBackground, at: 0)
+        vwEtymologyBackground.accessibilityIdentifier = "vwEtymologyBackground"
+        vwEtymologyBackground.backgroundColor = UIColor(named: "gray-500")
+//        vwEtymologyBackground.topAnchor.constraint(equalTo: vwVCHeaderOrangeTitle.bottomAnchor, constant: heightFromPct(percent: cardTopSpacing)).isActive=true
+//        vwEtymologyBackground.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: widthFromPct(percent: cardInteriorPadding)).isActive=true
+//        vwEtymologyBackground.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: widthFromPct(percent: cardInteriorPadding * -1)).isActive=true
+//        vwEtymologyBackground.heightAnchor.constraint(equalToConstant: heightFromPct(percent: 52)).isActive=true
+        vwEtymologyBackground.layer.cornerRadius = 10
+        
+        let stckVwEtymologyBackground = UIStackView()
+        stckVwEtymologyBackground.axis = .vertical
+        stckVwEtymologyBackground.spacing = 10
+        stckVwEtymologyBackground.translatesAutoresizingMaskIntoConstraints = false
+        vwEtymologyBackground.addSubview(stckVwEtymologyBackground)
+        stckVwEtymologyBackground.topAnchor.constraint(equalTo: vwEtymologyBackground.topAnchor, constant: heightFromPct(percent: 5)).isActive=true
+        stckVwEtymologyBackground.leadingAnchor.constraint(equalTo: vwEtymologyBackground.leadingAnchor, constant: widthFromPct(percent: 5)).isActive=true
+        stckVwEtymologyBackground.trailingAnchor.constraint(equalTo: vwEtymologyBackground.trailingAnchor, constant: widthFromPct(percent: -5)).isActive=true
+        stckVwEtymologyBackground.bottomAnchor.constraint(equalTo: vwEtymologyBackground.bottomAnchor, constant: heightFromPct(percent: -5)).isActive=true
+        
+        
+        let lblEtymologyTitle = UILabel()
+        lblEtymologyTitle.text = "Rincón"
+        lblEtymologyTitle.font = UIFont(name: "Rockwell_tu", size: 20)
+//        vwEtymologyBackground.addSubview(lblEtymologyTitle)
+        stckVwEtymologyBackground.addArrangedSubview(lblEtymologyTitle)
+        lblEtymologyTitle.accessibilityIdentifier = "lblEtymologyTitle"
+        lblEtymologyTitle.translatesAutoresizingMaskIntoConstraints=false
+//        lblEtymologyTitle.topAnchor.constraint(equalTo: vwEtymologyBackground.topAnchor, constant: heightFromPct(percent: 2.5)).isActive=true
+//        lblEtymologyTitle.leadingAnchor.constraint(equalTo: vwEtymologyBackground.leadingAnchor, constant: widthFromPct(percent: 5)).isActive=true
+        
+        let lblEtymology = UILabel()
+        lblEtymology.text = "Etymology"
+        lblEtymology.font = UIFont(name: "Rockwell-Bold_tu", size: 20)
+//        vwEtymologyBackground.addSubview(lblEtymology)
+        stckVwEtymologyBackground.addArrangedSubview(lblEtymology)
+        lblEtymology.accessibilityIdentifier = "lblEtymology"
+        lblEtymology.translatesAutoresizingMaskIntoConstraints=false
+//        lblEtymology.topAnchor.constraint(equalTo: lblEtymologyTitle.bottomAnchor, constant: heightFromPct(percent: 2.5)).isActive=true
+//        lblEtymology.leadingAnchor.constraint(equalTo: vwEtymologyBackground.leadingAnchor, constant: widthFromPct(percent: cardInteriorPadding)).isActive=true
+        
+        
+        let imgVwLine01 = createDividerLine(thicknessOfLine: 2)
+//        vwEtymologyBackground.addSubview(imgVwLine01)
+        stckVwEtymologyBackground.addArrangedSubview(imgVwLine01)
+        imgVwLine01.translatesAutoresizingMaskIntoConstraints=false
+        imgVwLine01.topAnchor.constraint(equalTo: lblEtymology.bottomAnchor, constant: heightFromPct(percent: 2.5)).isActive=true
+//        imgVwLine01.leadingAnchor.constraint(equalTo: vwEtymologyBackground.leadingAnchor, constant: widthFromPct(percent: cardInteriorPadding)).isActive=true
+//        imgVwLine01.trailingAnchor.constraint(equalTo: vwEtymologyBackground.trailingAnchor, constant: widthFromPct(percent: cardInteriorPadding * -1)).isActive=true
+        
+        
+        let lblEtymologyDef = UILabel()
+        lblEtymologyDef.text = "Spanish rincón, literally, corner, nook, alteration of recón, rencón from Arabic dialect (Spain) rukun (Arabic rukn)"
+        lblEtymologyDef.numberOfLines=0
+        lblEtymologyDef.lineBreakMode = .byWordWrapping
+        lblEtymologyDef.font = UIFont(name: "Rockwell_tu", size: 20)
+//        vwEtymologyBackground.addSubview(lblEtymologyDef)
+        stckVwEtymologyBackground.addArrangedSubview(lblEtymologyDef)
+        lblEtymologyDef.translatesAutoresizingMaskIntoConstraints=false
+//        lblEtymologyDef.topAnchor.constraint(equalTo: imgVwLine01.bottomAnchor, constant: heightFromPct(percent: 2.5)).isActive=true
+//        lblEtymologyDef.leadingAnchor.constraint(equalTo: vwEtymologyBackground.leadingAnchor, constant: widthFromPct(percent: cardInteriorPadding)).isActive=true
+//        lblEtymologyDef.trailingAnchor.constraint(equalTo: vwEtymologyBackground.trailingAnchor, constant: widthFromPct(percent: cardInteriorPadding * -1)).isActive=true
+        
+        
+        let lblFirstKnown = UILabel()
+        lblFirstKnown.text = "First Known Use"
+        lblFirstKnown.font = UIFont(name: "Rockwell-Bold_tu", size: 20)
+//        vwEtymologyBackground.addSubview(lblFirstKnown)
+        stckVwEtymologyBackground.addArrangedSubview(lblFirstKnown)
+        lblFirstKnown.translatesAutoresizingMaskIntoConstraints=false
+//        lblFirstKnown.topAnchor.constraint(equalTo: lblEtymologyDef.bottomAnchor, constant: heightFromPct(percent: 5)).isActive=true
+//        lblFirstKnown.leadingAnchor.constraint(equalTo: vwEtymologyBackground.leadingAnchor, constant: widthFromPct(percent: cardInteriorPadding)).isActive=true
+        
+        let imgVwLine02 = createDividerLine(thicknessOfLine: 2)
+//        vwEtymologyBackground.addSubview(imgVwLine02)
+        stckVwEtymologyBackground.addArrangedSubview(imgVwLine02)
+        imgVwLine02.translatesAutoresizingMaskIntoConstraints=false
+//        imgVwLine02.topAnchor.constraint(equalTo: lblFirstKnown.bottomAnchor, constant: heightFromPct(percent: 2.5)).isActive=true
+//        imgVwLine02.leadingAnchor.constraint(equalTo: vwEtymologyBackground.leadingAnchor, constant: widthFromPct(percent: cardInteriorPadding)).isActive=true
+//        imgVwLine02.trailingAnchor.constraint(equalTo: vwEtymologyBackground.trailingAnchor, constant: widthFromPct(percent: cardInteriorPadding * -1)).isActive=true
+        
+        let lblDate = UILabel()
+        lblDate.text = "1589"
+        lblDate.font = UIFont(name: "Rockwell_tu", size: 20)
+//        vwEtymologyBackground.addSubview(lblDate)
+        stckVwEtymologyBackground.addArrangedSubview(lblDate)
+        lblDate.translatesAutoresizingMaskIntoConstraints=false
+//        lblDate.topAnchor.constraint(equalTo: imgVwLine02.topAnchor, constant: heightFromPct(percent: 2.5)).isActive=true
+//        lblDate.leadingAnchor.constraint(equalTo: vwEtymologyBackground.leadingAnchor, constant: widthFromPct(percent: cardInteriorPadding)).isActive=true
+    }
+    
+    
     
     func setup_btnToRegister(){
         
@@ -222,6 +246,10 @@ class HomeVC: DefaultViewController {
         
         btnToRegister.translatesAutoresizingMaskIntoConstraints=false
         stckVwHome.addArrangedSubview(btnToRegister)
+        btnToRegister.sizeToFit()
+
+//        print("btnToRegister.size: \(btnToRegister.frame.size)")
+        btnToRegister.heightAnchor.constraint(equalToConstant: btnToRegister.frame.size.height + 30).isActive=true
 //        view.addSubview(btnToRegister)
 //        btnToRegister.topAnchor.constraint(equalTo: vwEtymologyBackground.bottomAnchor, constant: widthFromPct(percent: 5)).isActive=true
 //        btnToRegister.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: widthFromPct(percent: 5)).isActive=true
@@ -244,6 +272,8 @@ class HomeVC: DefaultViewController {
         btnToLogin.layer.borderWidth = 2
         btnToLogin.translatesAutoresizingMaskIntoConstraints=false
         stckVwHome.addArrangedSubview(btnToLogin)
+        btnToLogin.sizeToFit()
+        btnToLogin.heightAnchor.constraint(equalToConstant: btnToLogin.frame.size.height + 30).isActive=true
 //        view.addSubview(btnToLogin)
 //        btnToLogin.topAnchor.constraint(equalTo: btnToRegister.bottomAnchor, constant: widthFromPct(percent: 5)).isActive=true
 //        btnToLogin.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: widthFromPct(percent: 5)).isActive=true
@@ -258,14 +288,33 @@ class HomeVC: DefaultViewController {
     }
     
     func setup_pickerApi(){
+        let btnToWebsite = UIButton(type: .system)
+//        btnToWebsite.frame = CGRect(x: 20, y: 100, width: 300, height: 50)
+        btnToWebsite.setTitle("Tu Rincón Website", for: .normal)
+        btnToWebsite.addTarget(self, action: #selector(goToWebsite), for: .touchUpInside)
+//        if let unwp_font = UIFont(name: "Rockwell_tu", size: 20) {
+        btnToWebsite.titleLabel?.font = UIFont(name: "Rockwell_tu", size: 20)
+//        }
+        stckVwHome.addArrangedSubview(btnToWebsite)
+        
+        
+        let stckVwApi = UIStackView()
+        stckVwApi.translatesAutoresizingMaskIntoConstraints=false
+        stckVwApi.axis = .vertical
+        stckVwHome.addArrangedSubview(stckVwApi)
+        
         
         let lblApi = UILabel()
         lblApi.text = "Which Api?:"
         lblApi.translatesAutoresizingMaskIntoConstraints=false
-        stckVwHome.addArrangedSubview(lblApi)
+        stckVwApi.addArrangedSubview(lblApi)
+        lblApi.sizeToFit()
+        print("lblApi.frame.size: \(lblApi.frame.size)")
+        stckVwApi.heightAnchor.constraint(equalToConstant: lblApi.frame.size.height + 40).isActive=true
+//        lblApi.textAlignment = .
         
         
-        urlStore.baseString = "http://127.0.0.1:5001/"
+        urlStore.baseString = "https://dev.api.tu-rincon.com/"
         let indexDict = ["http://127.0.0.1:5001/":0,"https://dev.api.tu-rincon.com/":1,"https://api.tu-rincon.com/":2]
         
         let segmentedControl = UISegmentedControl(items: Environment.allCases.map { $0.rawValue })
@@ -273,13 +322,9 @@ class HomeVC: DefaultViewController {
         
         // Set initial selected segment
         segmentedControl.selectedSegmentIndex = indexDict[urlStore.baseString] ?? 0
-        stckVwHome.addArrangedSubview(segmentedControl)
+        stckVwApi.addArrangedSubview(segmentedControl)
         
-        let btnToWebsite = UIButton(type: .system)
-//        btnToWebsite.frame = CGRect(x: 20, y: 100, width: 300, height: 50)
-        btnToWebsite.setTitle("Go To Tu Rincón Web", for: .normal)
-        btnToWebsite.addTarget(self, action: #selector(goToWebsite), for: .touchUpInside)
-        stckVwHome.addArrangedSubview(btnToWebsite)
+
     }
     
     @objc func touchDown(_ sender: UIButton) {
